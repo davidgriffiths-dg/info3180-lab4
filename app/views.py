ff25fd2f2cc0743f102ce56ddaef71326937f1e1
David Griffiths <griffithsdavidchris@gmail.com>
2021-02-26 13:42:08 -0500

    Completed exercise 2

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ This file creates your application.
 """
 import os
 from app import app
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from werkzeug.utils import secure_filename
 from .forms import UploadForm
 
@@ -52,6 +52,27 @@ def upload():
         return render_template('upload.html', form=photoform)
 
     
+def get_uploaded_images():
+    lst = []
+    rootdir = os.getcwd()
+    for subdir, dirs, files in os.walk(rootdir +'/uploads/'):
+        for file in files:
+            lst.append(file)
+        return lst
+
+
+@app.route('/uploads/<filename>')
+def get_image(filename):
+    rootdir = os.getcwd()
+    return send_from_directory(os.path.join(rootdir,app.config['UPLOAD_FOLDER']), filename)
+
+
+@app.route('/files')
+def files():
+    if not session.get('logged_in'):
+        abort(401)
+    return render_template('files.html', images = get_uploaded_images())
+
 
 
 
